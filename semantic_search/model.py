41659744eb946997711da4965b01b7bf6d1c3fcb
Adhit <adhit.simhadri@gmail.com>
2025-07-24 23:36:18 +0530

    tested and written a version of semantic search that works

--- semantic_search/model.py
+++ semantic_search/model.py
@@ -1,26 +1,37 @@
-import torch
+import torch 
+import torch.nn as nn
+from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
-from transformers import AutoProcessor, AutoModel
 import requests
-import numpy as np
 
-class SemanticSearchModel:
-    def __init__(self,name='openai/clip-vit-base-patch32'):
-        self.model = AutoModel.from_pretrained(name, torch_dtype=torch.bfloat16, attn_implementation="sdpa")
-        self.processor =  AutoProcessor.from_pretrained(name)
+class SemanticSearchModel(nn.Module):
+    def __init__(self,name='openai/clip-vit-large-patch14'):
+        super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = CLIPModel.from_pretrained(name)
+        self.processor =  CLIPProcessor.from_pretrained(name)
+        self.eval()
+    
+    def forward(self,images,text):
+      inputs = self.processor(text=text, images=images, return_tensors="pt", padding=True).to(self.device)
+      outputs = self.model(**inputs)
+      logits_per_image = outputs.logits_per_image
+      probs = logits_per_image.softmax(dim=1) 
+      return probs
     
     def encode_image(self,images):
         if isinstance(images,list):
             images = [images]
-        
-        return self.processor(images=images,return_tensors='np')
+        inputs =  self.processor(images=images,return_tensors='pt')
+        image_features = self.model.get_image_features(**inputs)
+        return image_features
     
     def encode_text(self,text):
         if isinstance(text,list):
             text = [text]
-        
-        return self.processor(text=text,return_tensors='np')
+        inputs =  self.processor(text=text,return_tensors='pt')
+        text_features = self.model.get_text_features(**inputs)
+        return text_features
     
 
 
@@ -29,5 +40,9 @@ if __name__ == "__main__":
     image = Image.open(requests.get(url, stream=True).raw)
     text = 'cat'
     model = SemanticSearchModel()
+    with torch.no_grad():
+        text_embeds = model.encode_text(text)
+        image_embeds = model.encode_image(image)
+        print(torch.matmul(text_embeds, image_embeds.t().to(text_embeds.device)))
     
 
