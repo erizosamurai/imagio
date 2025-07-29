4f49be3b42e9f909d40a9e448ce0bbb07b31dbc1
Adhit <adhit.simhadri@gmail.com>
2025-07-30 01:26:35 +0530

    implemented faissdb

--- semantic_search/processor.py
+++ semantic_search/processor.py
@@ -1,7 +1,10 @@
-import os 
+import os
 from PIL import Image
+import numpy as np
 import torch
-from torch.utils.data import DataLoader
+import json
+import faiss
+from torch.utils.data import Dataset, DataLoader
 
 from model import SemanticSearchModel
 
@@ -19,42 +22,46 @@ class ImageDataset:
     return image, self.images[idx]
   
 class ImageProcessor:
-  def __init__(self,path,model:SemanticSearchModel=None, batch_size=32):
-      self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-      self.path = path
-      self.model = model or SemanticSearchModel()
-      self.batch_size = batch_size
+  def __init__(self,path,model:SemanticSearchModel=None, batch_size=32,dimension=768):
+    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    self.path = path
+    self.model = model or SemanticSearchModel()
+    self.batch_size = batch_size
+    self.index = faiss.IndexFlatIP(dimension)
 
   def process_folder(self):
-      dataset = ImageDataset(self.path)
-      dataloader = DataLoader(
-              dataset,
-              batch_size= self.batch_size,
-              shuffle=False,
-              num_workers=0,
-              collate_fn=lambda batch: list(zip(*batch)),  # To separate imgs, filenames
-          )
-      
-      all_embeddings = []
-      file_names = []
+    dataset = ImageDataset(self.path)
+    dataloader = DataLoader(
+            dataset,
+            batch_size= self.batch_size,
+            shuffle=False,
+            num_workers=4,
+            collate_fn=lambda batch: list(zip(*batch)),  # To separate imgs, filenames
+        )
 
-      print(f"Processing folder: {self.path}")
-      print(f"Using device: {self.device}")
+    all_embeddings = []
+    file_names = []
 
-      with torch.no_grad():
-          for imgs_batch,filename_batch in dataloader:
-              embeddings = self.model.encode_image(imgs_batch)
+    print(f"Processing folder: {self.path}")
+    print(f"Using device: {self.device}")
 
-              all_embeddings.append(embeddings.cpu())
-              file_names.extend(filename_batch)
+    with torch.no_grad():
+      for imgs_batch,filename_batch in dataloader:
+        embeddings = self.model.encode_image(imgs_batch)
 
-      embeddings_tensor = torch.cat(all_embeddings)
-      return embeddings_tensor, file_names
-  
+        all_embeddings.append(embeddings.cpu())
+        file_names.extend(filename_batch)
+
+    embeddings_tensor = torch.cat(all_embeddings)
+
+    return embeddings_tensor.detach().cpu().numpy(), file_names
+
   def save_embeddings(self,embeddings,file_names,save_path='embeddings/'):
       os.makedirs(save_path,exist_ok=True)
-      torch.save(embeddings,os.path.join(save_path,'embeddings.pt'))
-      torch.save(file_names,os.path.join(save_path,'filenames.pt'))
+      self.index.add(embeddings)
+      faiss.write_index(self.index, os.path.join(save_path, 'index.faiss'))
+      with open(os.path.join(save_path, 'filenames.json'), 'w') as f:
+        json.dump(file_names, f, indent=4)
   
 
 if __name__ == "__main__":
--- semantic_search/search.py
+++ semantic_search/search.py
@@ -1,20 +1,28 @@
-import torch
+import numpy as np
+import faiss
+import json 
 import os 
 from model import SemanticSearchModel
+
 
 class ImageSearch:
   def __init__(self,path='embeddings/',model:SemanticSearchModel=None):
     self.path = path
-    self.embeddings = torch.load(os.path.join(path,'embeddings.pt'),weights_only=False)
-    self.filenames = torch.load(os.path.join(path,'filenames.pt'),weights_only=False)
+    self.index= faiss.read_index(os.path.join(path,'index.faiss'))
+    with open(os.path.join(path, 'filenames.json'), 'r') as f:
+      self.filenames = json.load(f)
     self.model = model or SemanticSearchModel()
-  
-  def search(self,query,top_k=3):
+
+  def search(self,query,top_k=5,threshold=50):
     query_embedding = self.model.encode_text(query)
-    similarity = torch.matmul(query_embedding,self.embeddings.T).squeeze(0)
-    top_k_indices = similarity.topk(top_k).indices.tolist()
-    file_names = [self.filenames[i] for i in top_k_indices]
-    return file_names
+    query_embedding = query_embedding.detach().cpu().numpy().astype('float32').reshape(1, -1)
+    D, I = self.index.search(query_embedding, top_k)
+    results = [
+            (self.filenames[i], float(score))
+            for i, score in zip(I[0], D[0])
+            if score >= threshold
+        ]
+    return results
 
 
 if __name__ == "__main__":
